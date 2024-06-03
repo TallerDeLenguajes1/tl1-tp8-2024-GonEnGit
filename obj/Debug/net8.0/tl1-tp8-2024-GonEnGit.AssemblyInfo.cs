7e189436a6cc3c84bcc0588cd54fb3ec27fd04b6
Gonzalo Martinez <mgonzalo192@gmail.com>
2024-06-02 21:03:39 -0300

    sacaste la calculadora de la rama main

--- obj/Debug/net8.0/tl1-tp8-2024-GonEnGit.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-tp8-2024-GonEnGit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp8-2024-GonEnGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91162c113364a595d72eb1c80a143d8528ef1f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3797195dd512b3b2b35594c9fc383118461870")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp8-2024-GonEnGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp8-2024-GonEnGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
