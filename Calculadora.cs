4d352ebe158380e96e2f375e41eb4f407ad4a3da
Gonzalo Martinez <mgonzalo192@gmail.com>
2024-06-09 19:56:21 -0300

    por fin terminado TP08

--- Calculadora.cs
+++ Calculadora.cs
@@ -23,35 +23,79 @@ public class Operacion
     private double nuevoValor;
     private tipoOperacion operacion;
 
-    public double ResultadoAnterior { get => resultadoAnterior;}
-    public double NuevoValor { get => nuevoValor;}
+    public double ResultadoAnterior { get => resultadoAnterior; set => resultadoAnterior = value; }
+    public double NuevoValor { get => nuevoValor; set => nuevoValor = value; }
+    public tipoOperacion TipoOperacion { get => operacion; set => operacion = value; }
 
-    public enum tipoOperacion{ Suma, Resta, Multiplicacion, Division, }
+    public enum tipoOperacion{ 
+        Suma, 
+        Resta, 
+        Multiplicacion, 
+        Division,
+        Limpiar, }
 }
 public class Calculadora  // a veces se lo llama atributo
 {
+    List<Operacion> Historial = new List<Operacion>();
     private double dato;
-
-    public global::System.Double Resultado { get => dato; }
+    public double Resultado { get => dato; }
 
     public void Sumar(double termino)
     {
+    // se supone que guardas el resultado anterior,
+    // tendrais que guardar los datos que tenes primero
+    // y recien podes hacer la nueva operacion
+        AgregarAlHistorial(dato, termino, Operacion.tipoOperacion.Suma);
         dato += termino;
     }
     public void Restar(double termino)
     {
+        AgregarAlHistorial(dato, termino, Operacion.tipoOperacion.Resta);
         dato -= termino;
     }
     public void Multiplicar(double termino)
     {
+        AgregarAlHistorial(dato, termino, Operacion.tipoOperacion.Multiplicacion);
         dato *= termino;
     }
     public void Dividir(double termino)
     {
+        AgregarAlHistorial(dato, termino, Operacion.tipoOperacion.Division);
         dato /= termino;
     }
     public void Limpiar()
     {
+        AgregarAlHistorial(dato, 0, Operacion.tipoOperacion.Limpiar);
         dato = 0;
     }
+
+// agregas los nodos al historial de la misma forma que se hacia en C
+    public void AgregarAlHistorial(double resultado, double valor, Operacion.tipoOperacion operacion)
+    {
+        Operacion entrada = new Operacion();
+        entrada.ResultadoAnterior = resultado;
+        entrada.NuevoValor = valor;
+        entrada.TipoOperacion = operacion;
+
+        Historial.Add(entrada);
+    }
+
+// esta parte convierte el historial en una lista
+// de strings y devuelve eso en lugar del historial en si
+    public List<string> EnviarHistorial()
+    {
+        List<string> HistorialComoString = new List<string>();
+        foreach (var item in Historial)
+        {
+            string frase = $"Resultado Anterior: {item.ResultadoAnterior}, Valor ingresado: {item.NuevoValor}, Operacion: {item.TipoOperacion}";
+            HistorialComoString.Add(frase);
+        }
+        return HistorialComoString;
+    }
+
+    public void LiberarHistorial()
+    {
+        Historial.Clear();
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -10,73 +10,125 @@ Calculadora NuevaCalculadora = new Calculadora();
 
 do
 {
-    Console.WriteLine("Elija una operacion: ");
+// primero mostras el menu
+    Console.WriteLine("\nElija una operacion: ");
     Console.WriteLine("Suma (+)");
     Console.WriteLine("Resta (-)");
     Console.WriteLine("Producto (*)");
     Console.WriteLine("Cociente (/)");
     Console.WriteLine("Mostrar valor Actual (M)");
-    Console.WriteLine("Limpiar Valores (L)");
+    Console.WriteLine("Buscar en el historial (B)");
+    Console.WriteLine("Limpiar Valores (L)\n");
 
+// un control para ver que se ingresen las opciones correctas
     do
     {
         ingreso = Console.ReadLine();
         confirmacion = char.TryParse(ingreso, out operacion);
-        if (operacion != '+' && operacion != '-' && operacion != '*' && operacion != '/' && operacion != 'L' && operacion != 'M')
+        if (operacion != '+' && operacion != '-' && operacion != '*' && operacion != '/' && operacion != 'L' && operacion != 'M' && operacion != 'B')
         {
-            Console.WriteLine("Solo se pueden usar estas 4 operaciones.");
+            Console.WriteLine("\nSolo se pueden usar estas 6 operaciones.");
             confirmacion = false;
         }
     } while (!confirmacion);
 
-    Console.WriteLine("Ingrese un valor a usar: ");
-    do
+    if (operacion == 'M' || operacion == 'L' || operacion == 'B')
     {
-        ingreso = Console.ReadLine();
-        confirmacion = double.TryParse(ingreso, out numero);
-        if (!confirmacion)
+        if (operacion == 'M')
         {
-            Console.WriteLine("Ingrese unicamente numeros.");
+            Console.WriteLine($"\nEl resultado actual es {NuevaCalculadora.Resultado}\n");
         }
-    } while (!confirmacion);
-
-
-    switch (operacion)
-    {
-        case '+':
-            NuevaCalculadora.Sumar(numero);
-            break;
-
-        case '-':
-            NuevaCalculadora.Restar(numero);
-            break;
-        
-        case '*':
-            NuevaCalculadora.Multiplicar(numero);
-            break;
-
-        case '/':
-            NuevaCalculadora.Dividir(numero);
-            break;
-
-        case 'M':
-            Console.WriteLine($"El resultado actual es {NuevaCalculadora.Resultado}");
-            break;
-
-        case 'L':
+        else if (operacion == 'L')
+        {
             NuevaCalculadora.Limpiar();
-            break;
+        }
+        else
+        {
+        // te dijeron que no uses codigo edl lenguaje en las clases
+        // osea que la mejor opcion es hacer una lista de strings
+        // usando los datos del historial y manipular esa lista
+            List<string> HistorialTemp = new List<string>();
+            HistorialTemp = NuevaCalculadora.EnviarHistorial();
+
+            Console.WriteLine("\nIngrese 'Todo' para ver todo el historial.");
+            Console.WriteLine("O ingrese una operacion a buscar(Suma, Resta, Multiplicacion, Division, Limpiar): ");
+            ingreso = Console.ReadLine();
+
+            if (ingreso.Equals("Todo"))
+            {
+                foreach (var item in HistorialTemp)
+                {
+                    Console.WriteLine($"\n{item}");
+                }
+            }
+            else if (ingreso.Equals("Suma") || ingreso.Equals("Resta") 
+            || ingreso.Equals("Multiplicacion") || ingreso.Equals("Division") || ingreso.Equals("Limpiar"))
+            {
+                foreach (var item in HistorialTemp)
+                {
+                    if (item.Contains(ingreso))
+                    {
+                        Console.WriteLine($"\n{item}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nLa opcion ingresada no es valida.");
+            }
+        // aparentemente en C# liberar listas es un solo metodo
+        // la documentacion que encontraste en internet dice que
+        // si bien se podria, no hace falta liberar nodo por nodo
+            HistorialTemp.Clear();
+        }
+    }
+    else
+    {
+    // esta parte lleva a los metodos de Calculadora.cs
+        Console.WriteLine("\nIngrese un valor a usar: ");
+        do
+        {
+            ingreso = Console.ReadLine();
+            confirmacion = double.TryParse(ingreso, out numero);
+            if (!confirmacion)
+            {
+                Console.WriteLine("\nIngrese unicamente numeros.");
+            }
+        } while (!confirmacion);
+
+        switch (operacion)
+        {
+            case '+':
+                NuevaCalculadora.Sumar(numero);
+                break;
+
+            case '-':
+                NuevaCalculadora.Restar(numero);
+                break;
+            
+            case '*':
+                NuevaCalculadora.Multiplicar(numero);
+                break;
+
+            case '/':
+                NuevaCalculadora.Dividir(numero);
+                break;
+        }
     }
 
     do
     {
-        Console.WriteLine("Realizar otra operacion? (S/N)");
+        Console.WriteLine("\nRealizar otra operacion? (S/N)");
         ingreso = Console.ReadLine();
         confirmacion = char.TryParse(ingreso, out continuar);
         if (continuar != 'N' && continuar != 'S')
         {
-            Console.WriteLine("Ingrese S para continuar o N para terminar.");
+            Console.WriteLine("\nIngrese S para continuar o N para terminar.");
             confirmacion = false;
         }
     } while (continuar != 'N' && continuar != 'S');
 } while (continuar != 'N');
+
+NuevaCalculadora.LiberarHistorial();
+
+Console.WriteLine("\nHasta la proxima!\n");
